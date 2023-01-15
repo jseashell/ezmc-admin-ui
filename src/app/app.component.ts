5e94b9dc141cd3f6adeac80cf6b8b4b953756027
John Schellinger <john.schellinger@genesys.com>
2023-01-15 02:58:52 -0500

    Integrate admin service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,55 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+    private subs: Subscription[] = [];
+
+    constructor(private http: HttpClient) {}
+
     history: { timestamp: Date; message: string }[] = [];
+
+    ngOnInit(): void {}
 
     onStart(): void {
         this.history.unshift({
             timestamp: new Date(),
-            message: 'Game server started.',
+            message: 'Starting game server...',
         });
+        this.subs.push(
+            this.http.get(environment.adminService.start).subscribe((res: any) => {
+                this.history.unshift({
+                    timestamp: new Date(),
+                    message: res.message,
+                });
+                console.log('Started game server.', res);
+            }),
+        );
     }
 
     onStop(): void {
         this.history.unshift({
             timestamp: new Date(),
-            message: 'Game server stopped.',
+            message: 'Stopping game server...',
         });
+        this.subs.push(
+            this.http.get(environment.adminService.stop).subscribe((res: any) => {
+                this.history.unshift({
+                    timestamp: new Date(),
+                    message: res.message,
+                });
+                console.log('Stopped game server.', res);
+            }),
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.subs.forEach((sub) => sub?.unsubscribe());
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
     production: true,
+    adminService: {
+        start: 'https://mkn82d16q1.execute-api.us-east-1.amazonaws.com/prod/start',
+        stop: 'https://mkn82d16q1.execute-api.us-east-1.amazonaws.com/prod/stop',
+    },
 };
