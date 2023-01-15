00b6eceb9649e1b2d880238d0b84feb28cdc1c41
John Schellinger <john.schellinger@genesys.com>
2023-01-15 01:21:12 -0500

    Implement basic look and feel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ezmc-configurator';
+  history: { timestamp: Date; message: string }[] = [];
+
+  onStart(): void {
+    this.history.unshift({
+      timestamp: new Date(),
+      message: 'Game server started.',
+    });
+  }
+
+  onStop(): void {
+    this.history.unshift({
+      timestamp: new Date(),
+      message: 'Game server stopped.',
+    });
+  }
 }
