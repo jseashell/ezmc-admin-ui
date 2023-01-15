177738a6a5c9eaf33034abab233afd6ea553597a
John Schellinger <john.schellinger@genesys.com>
2023-01-15 14:54:55 -0500

    Disable start/stop buttons based on server status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, startWith, Subscription, switchMap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -8,11 +8,35 @@ import { environment } from 'src/environments/environment';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
     private subs: Subscription[] = [];
     history: { timestamp: Date; message: string }[] = [];
+    isRunning = false;
 
-    constructor(private http: HttpClient) {}
+    constructor(private cdr: ChangeDetectorRef, private http: HttpClient) {}
+
+    ngOnInit(): void {
+        // x number of milliseconds
+        this.subs.push(
+            interval(5000)
+                .pipe(
+                    // make first request right away; otherwise we have to wait x seconds before first one
+                    startWith(),
+
+                    // cancel previous request if it had not finished yet
+                    switchMap(() => {
+                        // catch the error inside to allow for a retry each time
+                        // you could also use `.catch` on the entire observable stream which will
+                        // cancel it if there's an error.
+                        return this.http.get(`${environment.adminService.url}/status`);
+                    }),
+                )
+                .subscribe((data: any) => {
+                    this.isRunning = data.isRunning;
+                    this.cdr.markForCheck();
+                }),
+        );
+    }
 
     onStart(): void {
         this.addToHistory('Starting game server...');
