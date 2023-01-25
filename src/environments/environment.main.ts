70e3bd14e00cd4f33ab96651742d651be0ea8075
John Schellinger <john.schellinger@genesys.com>
2023-01-25 18:51:28 -0500

    Update admin service url

--- src/environments/environment.main.ts
+++ src/environments/environment.main.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     adminService: {
-        url: 'https://76uogf99sl.execute-api.us-east-1.amazonaws.com/main',
+        url: 'https://76uogf99sl.execute-api.us-east-1.amazonaws.com/main/v1',
     },
 };
