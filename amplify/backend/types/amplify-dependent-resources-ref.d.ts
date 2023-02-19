9fc33a669f9c5c5cff96912eddcfaa30267ab7d6
John Schellinger <john.schellinger@genesys.com>
2023-02-19 12:29:13 -0500

    Configure Amplify backend and auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ezmcmanagementconsol07fa5b06": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
